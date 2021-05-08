ca8bb9c73d9376525f7dce2c1e9a02d4e400c4fd
Tushar-Mhaske <60687106+Tushar-Mhaske@users.noreply.github.com>
2021-05-08 22:29:08 +0530

    Create A5.java

---
+++ Lab Assignment 1/A5.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+import java.lang.*;
+
+import java.io.*;
+
+class A5
+
+{
+
+	public static void main (String[] args)	{
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter 1 no.");
+
+		int a =sc.nextInt();
+
+		System.out.println("2 no");
+
+		int b = sc.nextInt();
+
+		int multi=0;
+
+		multi = a*b; 
+
+	System.out.println(multi+" "); 
+
+	}
+
+}
