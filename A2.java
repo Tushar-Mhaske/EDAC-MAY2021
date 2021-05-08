cde5d4e87a755a1fa23d18f46aacaff47edc8807
Tushar-Mhaske <60687106+Tushar-Mhaske@users.noreply.github.com>
2021-05-08 14:42:00 +0530

    Add files via upload

---
+++ A2.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class A2
+{
+	public static void main (String[] args)
+	{
+		int a=500,b=10;
+		int sum =0;
+		sum= a+b;
+		System.out.println(sum);
+	}
+}
---
+++ A3.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Demo
+{
+	public static void main (String[] args)
+	{
+		int a=50,b=3,div=0;
+		div=50/3;
+		System.out.println(div);
+	}
+}
