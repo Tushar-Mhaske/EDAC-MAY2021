196a1f88b6af2592f8569bef54727a4f0d962a85
Tushar-Mhaske <60687106+Tushar-Mhaske@users.noreply.github.com>
2021-05-08 23:20:56 +0530

    Create A10.java

---
+++ Lab Assignment 1/A10.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+import java.lang.*;
+
+class A10 
+
+{
+
+  public static void main(String args[]) 
+
+  {
+
+    double i= (4.0 * (1 - (1.0/3) + (1.0/5) - (1.0/7) + (1.0/9) - (1.0/11)));
+
+    System.out.println(i);
+
+  } 
+
+  }
+
+
+ 
+
+
