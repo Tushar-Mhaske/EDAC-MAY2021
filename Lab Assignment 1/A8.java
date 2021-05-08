27f79a12a04af42a835078a979d00dd716210dc4
Tushar-Mhaske <60687106+Tushar-Mhaske@users.noreply.github.com>
2021-05-08 23:07:24 +0530

    Create A8.java

---
+++ Lab Assignment 1/A8.java
@@ -0,0 +1,16 @@
+public class A8
+
+{
+
+	public static void main(String[] args) {		
+        System.out.println("   J     A     v     v   a");
+
+        System.out.println("   j    a a     v   v   a a    ");
+
+        System.out.println("j  j   aaaaa     v v   aaaaa"); 
+
+        System.out.println(" jj   a     a     v   a     a "); 
+
+	}
+
+}
