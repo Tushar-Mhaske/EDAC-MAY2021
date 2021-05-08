affd1a693c6b9385432aa40ab56835df707d29cb
Tushar-Mhaske <60687106+Tushar-Mhaske@users.noreply.github.com>
2021-05-09 00:10:56 +0530

    Create A15.java

---
+++ Lab Assignment 1/A15.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class A15 
+
+{
+
+public static void main(String args[])
+
+{ 
+
+int a= 2,b=3,c;
+
+System.out.println("before"+a+""+b);
+
+c=a;
+
+a=b;
+
+b=c;
+
+System.out.println("After"+a+""+b);
+
+}
+
+}
