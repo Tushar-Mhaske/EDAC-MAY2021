a79df2699b11322c08e9b57fe5212586ddd87b46
Tushar-Mhaske <60687106+Tushar-Mhaske@users.noreply.github.com>
2021-05-08 22:52:55 +0530

    Create A7.java

---
+++ Lab Assignment 1/A7.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class A7
+
+{
+
+    public static void main(String args[])
+
+    
+
+{
+
+    Scanner sc = new Scanner(System.in);
+
+System.out.println("Enter no");
+
+int a = sc.nextInt();
+
+for(int i=1;i<=10;i++)
+
+{
+
+    int mul=0;
+
+    mul= a*i;
+
+System.out.println(mul);
+
+}
+
+}
+
+}
