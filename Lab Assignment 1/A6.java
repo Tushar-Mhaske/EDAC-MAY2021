7bee2fe6c3c166a2bb66692a8b978a1dacdb3935
Tushar-Mhaske <60687106+Tushar-Mhaske@users.noreply.github.com>
2021-05-08 22:39:20 +0530

    Create A6.java

---
+++ Lab Assignment 1/A6.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+import java.lang.*;
+
+import java.io.*;
+
+class A6
+
+{
+
+	public static void main (String[] args)	{
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter 1 no.");
+
+		int a =sc.nextInt();
+
+		System.out.println("2 no");
+
+		int b = sc.nextInt();
+
+		int multi=0;
+
+		multi = a*b; 
+
+	System.out.println("multi ="+multi+" ");
+
+	    int div, sub,mod; 
+
+	    div =a/b;
+
+	    sub =a-b;
+
+	    mod = a%b;
+
+	   System.out.println("div "+div+"sub= "+sub+"mod "+mod+" "); 
+
+	}
+
+}
