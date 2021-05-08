9098e00814716e63ce84c7fcc0fa09aed111c2c3
Tushar-Mhaske <60687106+Tushar-Mhaske@users.noreply.github.com>
2021-05-08 23:33:12 +0530

    Create A11.java

---
+++ Lab Assignment 1/A11.java
@@ -0,0 +1,21 @@
+class A11
+
+{ 
+
+	public static void main(String args[])	{
+
+		double pi = 3.14, r=7.5; 
+
+		double area = pi*r*r;
+
+		System.out.println(area);
+
+		double perimeter = 2*pi*r;
+
+		System.out.println(perimeter);
+
+		} 
+
+		}
+
+		
