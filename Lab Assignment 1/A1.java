29d888259e88da3b9e57016ef7d153545b8aebb9
Tushar-Mhaske <60687106+Tushar-Mhaske@users.noreply.github.com>
2021-05-08 14:55:45 +0530

    Add files via upload

---
+++ Lab Assignment 1/A1.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class A1
+{
+	public static void main (String[] args)
+	{
+		System.out.println("Hello World");
+		System.out.print("TUSHAR MHASKE");
+	}
+}
