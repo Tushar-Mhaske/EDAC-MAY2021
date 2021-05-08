83a3549e0d311d0d80619a759b0e697f96d60d6c
Tushar-Mhaske <60687106+Tushar-Mhaske@users.noreply.github.com>
2021-05-08 23:57:11 +0530

    Update A12.java

--- Lab Assignment 1/A12.java
+++ Lab Assignment 1/A12.java
@@ -1,1 +1,18 @@
-
+import java.util.*;
+class A12
+{
+public static void main(String args[])
+{
+ Scanner sc = new Scanner(System.in);
+int a = sc.nextInt();
+int b = sc.nextInt();
+int c = sc.nextInt();
+System.out.println("Enter val 1"); 
+System.out.println("Enter val 2"); 
+System.out.println("Enter val 3"); 
+int Total = a+b+c;
+System.out.println(Total);
+int avg =Total/3;
+System.out.println(avg);
+}
+}
