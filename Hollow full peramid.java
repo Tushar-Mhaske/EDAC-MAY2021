98464fe5f4564cbc18218c7315bb58dcdb6ae9e5
Tushar-Mhaske <60687106+Tushar-Mhaske@users.noreply.github.com>
2021-05-09 14:18:34 +0530

    Create Hollow full peramid.java

---
+++ Hollow full peramid.java
@@ -0,0 +1,18 @@
+class Main
+{ 
+	public static void main(String args[])
+	{
+		for(int i=1;i<=7;i++)
+		{
+			for(int j=1;j<=11;j++)
+			{
+				if(i==6||i+j==7|| j-i==5){
+				System.out.print("*");}
+				else{
+System.out.print(" ");
+				}
+				}
+				System.out.println( );
+				}
+				}
+				}
